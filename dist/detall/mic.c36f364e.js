c3729fa517438e221fb96f9dd1488b7bf0e56552
Roser Fuster Bertomeu <rfuster@uoc.edu>
2023-04-17 21:52:20 +0200

    first commit

---
+++ dist/detall/mic.c36f364e.js
@@ -0,0 +1,29 @@
+var index = 0;
+var slides = document.getElementsByClassName("slider");
+var slidesAuto = document.getElementsByClassName("sliderAutomatic");
+if (slides.length != 0) slides[index].style.display = "block";
+if (slidesAuto.length != 0) {
+    slidesAuto[index].style.display = "block";
+    sliderAuto();
+}
+function sliderAuto() {
+    slidesAuto[index].style.display = "none";
+    index += 1;
+    if (index > slidesAuto.length - 1) index = 0;
+    slidesAuto[index].style.display = "block";
+    setTimeout(sliderAuto, 5000);
+}
+function next() {
+    slides[index].style.display = "none";
+    index += 1;
+    if (index > slides.length - 1) index = 0;
+    slides[index].style.display = "block";
+}
+function prev() {
+    slides[index].style.display = "none";
+    index -= 1;
+    if (index < 0) index = slides.length - 1;
+    slides[index].style.display = "block";
+}
+
+//# sourceMappingURL=mic.c36f364e.js.map
